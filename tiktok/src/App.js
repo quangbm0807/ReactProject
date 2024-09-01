294eada457c5a9aaf9db216b72aeb9c4297c76e6
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-09-01 17:59:22 +0700

    Ok

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
+
+
+
+
+const orders = [100, 200, 300]
 function App() {
+  const [counter, setCounter] = useState(() => {
+    const total = orders.reduce((total, cur) => total + cur)
+    return total
+  });
+
+  const handleIncrease = () => {
+    // setCounter(counter + 1);
+    setCounter(preCount => preCount + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
