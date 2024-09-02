12d3288cba2dc8a8720438aa07f5256ee64a1705
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-09-02 10:05:30 +0700

    Add TwoWay Binding

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+
+
+// const gifts = ['CPU i9',
+//   'RAM 16GB',
+//   'SSD 512GB'
+// ];
+
+// function App() {
+//   // const [count, setCount] = useState(0);
+
+//   // const handleClick = () => {
+//   //   setCount(count=>count+1);
+//   // };
+//   const  [gift, setGift] = useState();
+
+//   const randomGift = () => {
+//     const index = Math.floor(Math.random() * gifts.length);
+//     setGift(gifts[index]);
+//   };
+//   return (
+//     <div style={{ padding: 20 }}>
+//       <h1>{gift || 'Chưa có phần thưởng'}</h1>
+//       <button onClick={randomGift} > Lấy thưởng</button>
+//     </div>
+//   );
+// }
+
+const courses = [
+  {
+    id: 1,
+    name: 'HTML, CSS'
+  },
+  {
+    id: 2,
+    name: 'Javascript'
+  },
+  {
+    id: 3,
+    name: 'ReactJS'
+  }
+]
+
 
 function App() {
+  const [checked, setChecked] = useState([]);
+  const handleSubmit = () => {
+    console.log({ ids: checked })
+  };
+
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      const isChecked = checked.includes(id);
+      if (isChecked) {
+        return checked.filter(item => item !== id);
+      }
+
+      return [...prev, id];
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      {courses.map(course => (
+        <div key={course.id}>
+          <label>
+            <input
+              type="checkbox"
+              name="course"
+              value={course.id}
+              onChange={() => handleCheck(course.id)}
+              checked={checked.includes(course.id)}
+            />
+            {course.name}
+          </label>
+        </div>
+      ))}
+      <button onClick={handleSubmit} type="submit">Register</button>
     </div>
   );
 }
 
+
+
 export default App;
+
