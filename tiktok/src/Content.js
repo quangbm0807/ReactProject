d41b3ede8fa9ec138ab813165217c2e29101f255
Quang <135381994+quangbm0807@users.noreply.github.com>
2024-09-03 18:28:28 +0700

    Add Use Effect

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -5,23 +5,28 @@ import { useState, useEffect } from 'react';
 
 function Content() {
     const tabs = ['posts', 'comments', 'albums', 'photos', 'todos', 'users'];
-    const [posts, set] = useState([]);
+    const [data, setData] = useState([]);
     const [type, setType] = useState('posts');
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
-            .then(post => {
-                set(post);
-            }
-            )
-    }, []);
-    console.log(type);
+            .then(data => {
+                setData(data);
+            });
+    }, [type]);
     return (
         <>
             <h1>Content</h1>
-            {tabs.map((tab, index) => <button onClick={e=>setType(tabs[index])} style={{ padding: 20 }} key={index}>{tab}</button>)}
+            {tabs.map((tab, index) =>
+                <button
+                    onClick={e => setType(tabs[index])}
+                    style={{ padding: 20, backgroundColor: type === tab ? 'red' : '' }}
+                    key={index}
+                >{tab}
+                </button>)}
+
             <ul>
-                {posts.map(post => <li key={post.id}>{post.title}</li>)}
+                {data.map(post => <li key={post.id}>{post.title || post.name}</li>)}
             </ul>
 
         </>
